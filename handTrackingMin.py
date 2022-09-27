9c4d56a74567281643114a2a05fe7d25c09c0199
Niraj Kumar <nirajk23523@gmail.com>
2022-09-27 20:16:30 +0530

    basic hand tracking and fps counter

--- handTrackingMin.py
+++ handTrackingMin.py
@@ -1,19 +1,38 @@
-import cv2
+import cv2 as cv
 import mediapipe as mp
 import time
 
-cap = cv2.VideoCapture(0)
+cap = cv.VideoCapture(0)
 
 if not cap.isOpened():
     print("Cannot open camera")
     exit()
 
+mpHands = mp.solutions.hands
+mpDraw = mp.solutions.drawing_utils
+hands = mpHands.Hands()
+
+pTime, cTime = 0, 0
+
 while True:
     success, img = cap.read()
 
-    cv2.imshow("Made By Me", img)
-    if cv2.waitKey(1) == ord('q'):
+    imgRGB = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+    results = hands.process(imgRGB)
+
+    if results.multi_hand_landmarks:
+        for handLms in results.multi_hand_landmarks:
+            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+
+    cTime = time.time()
+    fps = int(1 / (cTime - pTime))
+    pTime = cTime
+
+    cv.putText(img, str(fps), (10, 70), cv.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
+
+    cv.imshow("Made By Me", img)
+    if cv.waitKey(1) == ord('q'):
         break
 
 cap.release()
-cv2.destroyAllWindows()
+cv.destroyAllWindows()
